9597b79d88556abc3716819a7008ed61abe4f18b
wws <1274704958@qq.com>
2023-11-01 14:54:06 +0800

    AutoAppendMsgOnIdle

--- BililiveDebugPlugin/BililiveDebugPlugin.cs
+++ BililiveDebugPlugin/BililiveDebugPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Threading;
 using BilibiliDM_PluginFramework;
+using Interaction;
 using InteractionGame;
 
 namespace BililiveDebugPlugin
@@ -11,6 +12,9 @@ namespace BililiveDebugPlugin
             InteractionGame.MsgGiftParser<DebugPlugin>,
             Interaction.DefAoe4Bridge, DebugPlugin> messageDispatcher;
 
+        private Action<DyMsg> m_AppendMsgAction;
+        private DateTime m_AutoAppendMsgTime;
+        public static readonly bool AutoAppendMsgOnIdle = true;
         public DebugPlugin()
         {
             ReceivedDanmaku += OnReceivedDanmaku;
@@ -48,6 +52,45 @@ namespace BililiveDebugPlugin
             base.Stop();
             messageDispatcher.Stop();
             messageDispatcher = null;
+        }
+
+        public void OnInit(Action<DyMsg> appendMsgAction)
+        {
+            m_AppendMsgAction = appendMsgAction;
+            m_AutoAppendMsgTime = DateTime.Now;
+        }
+
+        public void OnStop()
+        {
+            m_AppendMsgAction = null;
+        }
+
+        public void OnTick()
+        {
+            if (AutoAppendMsgOnIdle)
+            {
+                var span = DateTime.Now - m_AutoAppendMsgTime;
+                if (span.TotalSeconds >= 20)
+                {
+                    AppendRandomMsg(0, 0, 7, 1, 20);
+                    AppendRandomMsg(1, 0, 7, 1, 20);
+                    m_AutoAppendMsgTime = DateTime.Now;
+                }
+            }
+        }
+
+        private void AppendRandomMsg(int p, int sid_s, int sid_e, int num_s, int num_e)
+        {
+            var num = new Random((int)DateTime.Now.Ticks).Next(num_s, num_e);
+            for(int i = 0;i < num;i++)
+            {
+                m_AppendMsgAction?.Invoke(new DyMsg(){ Player = p, Msg = new Random((int)DateTime.Now.Ticks).Next(sid_s,sid_e + 1) });
+            }
+        }
+
+        public void OnAppendMsg(DyMsg msg)
+        {
+            m_AutoAppendMsgTime = DateTime.Now;
         }
     }
 }

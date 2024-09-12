e533b7f6ebad422d07876a65c21fa600304f2db8
eqd <1274704958@qq.com>
2024-09-12 19:49:35 +0800

    EvaluateExpr

--- BarPlugin/Utils/Utils.cs
+++ BarPlugin/Utils/Utils.cs
@@ -29,23 +29,22 @@ namespace Utils
                 {
                     if(param != null && param.Parameters.Length == 2 )
                     {
-                        var p1 = param.Parameters[0].ParsedExpression.ToString();
-                        var p2 = param.Parameters[1].ParsedExpression.ToString();
-                        if (int.TryParse(p1.Substring(2,p1.Length - 3),out var idx) && idx < args.Length)
+                        var p1 = param.Parameters[0].Evaluate();
+                        var p2 = param.Parameters[1].Evaluate().ToString();
+                        if (p1 != null)
                         {
-                            p2 = p2.Substring(1, p2.Length - 2);
-                            var ty = args[idx - 1].GetType();
+                            var ty = p1.GetType();
                             var prop = ty.GetProperty(p2);
                             if (prop != null)
                             {
-                                var res = prop.GetValue(args[idx - 1], null);
+                                var res = prop.GetValue(p1, null);
                                 param.Result = res;
                                 return;
                             }
                             var field = ty.GetField(p2);
                             if (field != null)
                             {
-                                var res = field.GetValue(args[idx - 1]);
+                                var res = field.GetValue(p1);
                                 param.Result = res;
                                 return;
                             }
